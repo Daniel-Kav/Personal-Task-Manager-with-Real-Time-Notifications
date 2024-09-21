d829fa5ebaa5cb85c3f0a882112e4d9c5425e6d6
daniel-kav <dankavatha01@gmail.com>
2024-09-21 11:28:07 +0300

    tasklist api view

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user = self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
+    
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    
