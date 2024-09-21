935531f6c1e9a2378ea11c820116fa4b8b999100
daniel-kav <dankavatha01@gmail.com>
2024-09-21 13:08:51 +0300

    make sure user viewing darshboard is authorized

--- frontend-app/src/components/Darshboard.tsx
+++ frontend-app/src/components/Darshboard.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (!token) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+
   return (
     <div>
       <h1>Welcome to the Dashboard</h1>
