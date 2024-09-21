c1eb99fbd7d4832c0a9c586fcc9f2c11905335fc
daniel-kav <dankavatha01@gmail.com>
2024-09-22 01:18:10 +0300

    Logout Route

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Darshboard';
+import Logout from './components/Logout';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,8 @@ const App: React.FC = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/logout" element={<Logout />} />
+
       </Routes>
     </Router>
   );
