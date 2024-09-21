5fdc5f92b1d99ea756a9417cbb2778350ebbfefb
daniel-kav <dankavatha01@gmail.com>
2024-09-22 01:40:31 +0300

    registration page api intergration

--- frontend-app/src/components/Register.tsx
+++ frontend-app/src/components/Register.tsx
@@ -1,13 +1,35 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Register: React.FC = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [ error, setError] = useState<string |null>(null);
+  const navigate = useNavigate()
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      setError('Password does not match');
+      return
+    }
     // Send a POST request to your Django API
+    try{
+      const response = await axios.post('http://localhost:8080/api/register',{
+        username, password}
+      );
+
+      if (response.status === 200) {
+        navigate('/login')
+      }
+    } catch (error) {
+      console.error('registration failed ',error);
+      setError('Registration failed. Please try again')
+    }
     console.log('Registering with', username, email, password);
   };
 
@@ -39,6 +61,14 @@ const Register: React.FC = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
+        <div>
+          <label >confirmPassword</label>
+          <input 
+            type="text"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          </div>
         <button type="submit">Register</button>
       </form>
     </div>
