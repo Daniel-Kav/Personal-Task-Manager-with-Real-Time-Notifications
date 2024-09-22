0c442bdfbe67f9b882477bc224a5e920197b802e
daniel-kav <dankavatha01@gmail.com>
2024-09-22 17:29:43 +0300

    tailwind config

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -4,6 +4,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Darshboard';
 import Logout from './components/Logout';
+import TestComponent from './components/Test';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="/" element={<TestComponent />} />
 
       </Routes>
     </Router>
